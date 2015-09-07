488d24fd2aa94208666e144c41867019f6e10937
Dean Radcliffe <deanmisc@yahoo.com>
2015-09-06 21:57:47 -0500

    Changes for Meteor 1.2 - ecmascript package not yet ready to depend upon

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
-  api.use('ddp', 'client', 'promise');
+  api.use('ddp', 'client', 'promise', 'ecmascript');
   api.addFiles('promise.js', ['client', 'server']);
   api.addFiles('reactivePromise.js', 'client');
   api.export("ReactivePromise", 'client');
@@ -18,11 +18,11 @@ Package.onTest(function(api) {
   api.use('tinytest');
   api.use('okgrow:promise');
   api.use('reactive-var');
-  api.use('grigio:babel');
-  api.addFiles('tests/support.es6.js', ['client', 'server']);
-  api.addFiles('tests/identity-tests.es6.js', ['client', 'server']);
-  api.addFiles('tests/chaining-tests.es6.js', ['client', 'server']);
-  api.addFiles('tests/error-tests.es6.js', ['client', 'server']);
-  api.addFiles('tests/call-wo-callback.es6.js', ['client']);
-  api.addFiles('tests/reactive-promise.es6.js', ['client']);
+  api.use('ecmascript');
+  api.addFiles('tests/support.js', ['client', 'server']);
+  api.addFiles('tests/identity-tests.js', ['client', 'server']);
+  api.addFiles('tests/chaining-tests.js', ['client', 'server']);
+  api.addFiles('tests/error-tests.js', ['client', 'server']);
+  api.addFiles('tests/call-wo-callback.js', ['client']);
+  api.addFiles('tests/reactive-promise.js', ['client']);
 });
